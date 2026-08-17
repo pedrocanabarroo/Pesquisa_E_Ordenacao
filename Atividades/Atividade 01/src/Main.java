6cbd39fc52be8d5679fb9476c285a9265162ae43
Pedro Henrique de B. Canabarro <111590901+pedrocanabarroo@users.noreply.github.com>
2026-08-17 08:06:23 -0300

    Add Main class to execute OrdenacaoController

---
+++ Atividades/Atividade 01/src/Main.java
@@ -0,0 +1,12 @@
+import controller.OrdenacaoController;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        OrdenacaoController controller =
+                new OrdenacaoController();
+
+        controller.executar();
+    }
+}
